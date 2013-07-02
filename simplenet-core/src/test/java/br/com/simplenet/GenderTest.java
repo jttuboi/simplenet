ec96321cad56b25ea1af9027c6d7fad1ebb118c3
Jorge Avelar <jorgecavelar@gmail.com>
2013-07-02 00:40:14 -0300

    add test case for enums

---
+++ simplenet-core/src/test/java/br/com/simplenet/GenderTest.java
@@ -0,0 +1,19 @@
+package br.com.simplenet;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Test;
+
+public class GenderTest extends TestCase{
+	
+	@After
+	public void setUp(){
+		
+	}
+
+	@Test
+	public void testSetValue() {
+		assertTrue(true);
+	}
+}
